f246358c1b5df1de5c20000b463d76cfe1e1d75f
Marius Fogelgren <74210716+F0gsler@users.noreply.github.com>
2024-11-04 10:40:44 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPOp1
+{
+    internal class Class1
+    {
+        public int count(string text, string word)
+        {
+            
+            int times = 0;
+            int start = text.ToLower().IndexOf(word.ToLower());
+
+            while (start != -1)
+            {
+                times++;
+                start = text.ToLower().IndexOf(word.ToLower(), start + 1);
+            }
+            return times;
+
+        }
+    }
+}
